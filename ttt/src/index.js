f3b36efaff94cdc1d7973d0d89cc133248a2a07f
jefzli21 <jefzli21@gmail.com>
2022-08-09 16:56:00 -0700

    done

--- ttt/src/index.js
+++ ttt/src/index.js
@@ -1,6 +1,10 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js')// require appropriate file
+const Game = require('../ttt_node/game.js')// require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  let el = document.querySelector(".ttt")
+  let game = new Game()
+  let view = new View(game, el)
+  
 });
--- ttt/src/ttt-view.js
+++ ttt/src/ttt-view.js
@@ -1,14 +1,59 @@
+const Game = require("../ttt_node/game");
+
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+    this.handleClick = this.handleClick.bind(this);
+    this.bindEvents();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+
+    let ul = document.createElement('ul')
+    for(let i =0;i<3;i++){
+      for(let j=0; j<3; j++){
+        let li = document.createElement('li');
+        li.dataset.pos = JSON.stringify([i,j]);
+        // li.innerText= "ha"
+        ul.append(li)
+      }
+    }
+    this.el.append(ul)
+  }
   
-  bindEvents() {}
+  bindEvents() {
+    this.el.addEventListener("click", this.handleClick)
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+   let el = e.target
+   if(el.tagName.toLowerCase() === 'li'){
+     this.makeMove(el)
+   }
+  }
 
-  makeMove(square) {}
+  makeMove(square) {
+    let pos = JSON.parse(square.dataset.pos);
+    this.game.playMove(pos);
+    square.classList.add(this.game.currentPlayer);
+    if(this.game.isOver()){
+      if(this.game.winner() !== null){
+        this.handleGameOver()
+      }else{
+        alert("DRAW!!")
+      }
+      location.reload()
 
+    }
+  }
+  
+  handleGameOver(){
+
+    alert(`player ${this.game.currentPlayer} won! but you're a 🤡`)
+  }
 }
+
 
 module.exports = View;
